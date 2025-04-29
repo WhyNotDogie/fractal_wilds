8b9d7176cda943f172920d7663954589e04a9e72
WhyNotDogie <112395091+WhyNotDogie@users.noreply.github.com>
2025-04-28 21:48:48 -0700

    im going to fungking die of death

--- common/src/main/java/dev/dogie/fractal_wilds/CommonClass.java
+++ common/src/main/java/dev/dogie/fractal_wilds/CommonClass.java
@@ -10,6 +10,8 @@ import net.minecraft.world.item.Items;
 // however it will be compatible with all supported mod loaders.
 public class CommonClass {
 
+
+
     // The loader specific projects are able to import and use any code from the common project. This allows you to
     // write the majority of your code here and load it from your loader specific projects. This example has some
     // code that gets invoked by the entry point of the loader specific projects.
---
+++ common/src/main/java/dev/dogie/fractal_wilds/worldgen/FWChunkGenerator.java
@@ -0,0 +1,94 @@
+package dev.dogie.fractal_wilds.worldgen;
+
+import com.mojang.datafixers.kinds.App;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.WorldGenRegion;
+import net.minecraft.world.level.LevelHeightAccessor;
+import net.minecraft.world.level.NoiseColumn;
+import net.minecraft.world.level.StructureManager;
+import net.minecraft.world.level.biome.BiomeManager;
+import net.minecraft.world.level.biome.BiomeSource;
+import net.minecraft.world.level.chunk.ChunkAccess;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.levelgen.RandomState;
+import net.minecraft.world.level.levelgen.blending.Blender;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+// if i disappear tomorrow you know why
+
+public class FWChunkGenerator extends ChunkGenerator {
+
+    public static final MapCodec<FWChunkGenerator> CODEC = ;
+
+    public FWChunkGenerator(BiomeSource biomeSource) {
+        super(biomeSource);
+    }
+
+    @Override
+    protected MapCodec<FWChunkGenerator> codec() {
+        return CODEC;
+    }
+
+    @Override
+    public void applyCarvers(WorldGenRegion worldGenRegion, long l, RandomState randomState, BiomeManager biomeManager, StructureManager structureManager, ChunkAccess chunkAccess, GenerationStep.Carving carving) {
+
+    }
+
+    @Override
+    public void buildSurface(WorldGenRegion worldGenRegion, StructureManager structureManager, RandomState randomState, ChunkAccess chunkAccess) {
+
+    }
+
+    @Override
+    public void spawnOriginalMobs(WorldGenRegion worldGenRegion) {
+
+    }
+
+    @Override
+    public int getGenDepth() {
+        return 0;
+    }
+
+    @Override
+    public CompletableFuture<ChunkAccess> fillFromNoise(Blender blender, RandomState randomState, StructureManager structureManager, ChunkAccess chunkAccess) {
+        return null;
+    }
+
+    @Override
+    public int getSeaLevel() {
+        return 0;
+    }
+
+    @Override
+    public int getMinY() {
+        return 0;
+    }
+
+    @Override
+    public int getBaseHeight(int i, int i1, Heightmap.Types types, LevelHeightAccessor levelHeightAccessor, RandomState randomState) {
+        return 0;
+    }
+
+    @Override
+    public NoiseColumn getBaseColumn(int i, int i1, LevelHeightAccessor levelHeightAccessor, RandomState randomState) {
+        return null;
+    }
+
+    @Override
+    public void addDebugScreenInfo(List<String> list, RandomState randomState, BlockPos blockPos) {
+
+    }
+
+    public int getWorldHeight() {
+        return 256;
+    }
+
+}
