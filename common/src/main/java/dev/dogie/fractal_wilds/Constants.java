0c68976503ded4e6e63e328823af338e976b9a6d
WhyNotDogie <112395091+WhyNotDogie@users.noreply.github.com>
2025-04-28 18:02:03 -0700

    the great 2025 examplemod whitewashing also i deleted .vscode

---
+++ common/src/main/java/dev/dogie/fractal_wilds/Constants.java
@@ -0,0 +1,11 @@
+package dev.dogie.fractal_wilds;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "fractal_wilds";
+	public static final String MOD_NAME = "Fractal Wilds";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
